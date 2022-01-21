53218228ebb3e8f4b7ec178fc2766438e542f6be
dounya-m <mellouki.dounya@gmail.com>
2022-01-21 20:04:44 +0100

    v1.0

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=he, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class="logo">
+        <img src="logo.png" alt="Logo">
+    </header>
+    <section class="head">
+        <div class="discription">
+            <h1>Manage Your Stock Now.</h1>
+            <p>our application helpe you to manage your stock easy without
+                difficult you can control your stock, see your selles, 
+                manages your products and control your employees.  </p>
+                <button type="submit"><a href="login.php"> Get Started</a></button>
+                <div class="horizontal">
+                <div class="verticales">
+                    <p></p><p></p><p></p><p></p><p></p><p></p><p></p>
+                    <p></p><p></p><p></p><p></p><p></p><p></p><p></p>
+                    <p></p><p></p><p></p><p></p><p></p><p></p>
+                </div>
+                <div class="verticales">
+                    <p></p><p></p><p></p><p></p><p></p><p></p><p></p>
+                    <p></p><p></p><p></p><p></p><p></p><p></p><p></p>
+                    <p></p><p></p><p></p><p></p><p></p><p></p>
+                </div>
+                <div class="verticales">
+                    <p></p><p></p><p></p><p></p><p></p><p></p><p></p>
+                    <p></p><p></p><p></p><p></p><p></p><p></p><p></p>
+                    <p></p><p></p><p></p><p></p><p></p><p></p>
+                </div>
+                <div class="verticales">
+                    <p></p><p></p><p></p><p></p><p></p><p></p><p></p>
+                    <p></p><p></p><p></p><p></p><p></p><p></p><p></p>
+                    <p></p><p></p><p></p><p></p><p></p><p></p>
+                </div>
+                <div class="verticales">
+                    <p></p><p></p><p></p><p></p><p></p><p></p><p></p>
+                    <p></p><p></p><p></p><p></p><p></p><p></p><p></p>
+                    <p></p><p></p><p></p><p></p><p></p><p></p>
+                </div>
+                <div class="verticales">
+                    <p></p><p></p><p></p><p></p><p></p><p></p><p></p>
+                    <p></p><p></p><p></p><p></p><p></p><p></p><p></p>
+                    <p></p><p></p><p></p><p></p><p></p><p></p>
+                </div>
+                <div class="verticales">
+                    <p></p><p></p><p></p><p></p><p></p><p></p><p></p>
+                    <p></p><p></p><p></p><p></p><p></p><p></p><p></p>
+                    <p></p><p></p><p></p><p></p><p></p><p></p>
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="images">
+            <div class="cercle"></div>
+            <div class="square"></div>
+            <img src="https://images.unsplash.com/photo-1542744173-8e7e53415bb0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="images">
+        </div>
+    </section>
+    <main>
+        <section class="first">
+            <div class="title">
+                <h3>How does it work?</h3>
+                <p>As simple as opening a can</p>
+            </div>
+            <div class="icones">
+                <div class="creat">
+                    <span class="iconify" data-icon="akar-icons:person-add" style="color: #020076; font-size: 95px;"></span>
+                    <p>create your account</p>
+                </div>
+                <div class="add">
+                    <span class="iconify" data-icon="akar-icons:folder-add" style="color: #020076; font-size: 95px;"></span>
+                    <p>Add your product</p>
+                </div>
+                <div class="manage">
+                    <span class="iconify" data-icon="ic:baseline-manage-history" style="color: #020076; font-size: 95px;"></span>
+                    <p>Manage your products</p>
+                </div>
+            </div>
+        </section>
+        <section class="second">
+            <div class="title">
+                <h3>How can we help your Business ?</h3>
+                <p>We build readymade websites, 
+                    mobile applications,<br> and elaborate
+                    online business <br>services.</p>
+            </div>
+            <div class="midlle">
+                <div class="first-line">
+                    <div class="blue">
+                        <span class="iconify" data-icon="fluent:box-multiple-search-24-regular" style="color: #2639ed; font-size: 110px;"></span>
+                        <h4>Business Idea Planning</h4>
+                        <p>We present you a proposal and discuss niffty-gritty like</p>
+                    </div>
+                    <div class="pink">
+                        <span class="iconify" data-icon="gala:file-code1" style="color: #ff007a; font-size: 110px;"></span>
+                        <h4>Development Website and App</h4>
+                        <p>Communication protocols apart from engagement models</p>
+                    </div>
+                </div>
+                <div class="second-line">
+                    <div class="green">
+                        <span class="iconify" data-icon="feather:pie-chart" style="color: #00da71; font-size: 110px;"></span>
+                        <h4>Market Analysis Project</h4>
+                        <p>Protocols apart from aengage models, pricing billing</p>
+                    </div>
+                    <div class="orange">
+                        <span class="iconify" data-icon="bx:bx-wallet" style="color: #f90; font-size: 110px;"></span>
+                        <h4>Financial Planning System</h4>
+                        <p>Protocols apart from aengage models, pricing billing</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="how-work">
+            <div class="img-discription">
+                <img src="https://images.unsplash.com/photo-1598870784088-35e7058da12c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" alt="image">
+                <p class="yellow-cercle"></p>
+            </div>
+            <div class="text-discription">
+                <h3>How our application work.</h3>
+                <p>Le Lorem Ipsum est simplement du faux texte 
+                    employé dans la composition . Le Lorem Ipsum 
+                    est le faux texte standard de l'imprimerie depuis 
+                    es 1500,ensemble des morceaux de texte pour réaliser.</p>
+                    <p class="blue-square"></p>
+            </div>
+        </section>
+        <section class="contacts">
+            <diV class="left">
+                <h3>Subscribe Newsletter</h3>
+                <p>I will update good news and promotion service not spam</p>
+            </diV>
+            <div class="style">
+            <diV class="right">
+              <div> <input class="mail" type="email" id="email" name="email" placeholder="Enter your email address.."> </div>
+                <div><input type="submit" id="submit" class="submit"  value="Contact Now"></div>
+            </diV>
+        </div>
+        </section>
+    </main>
+    <footer>
+        <div class="footer-contain">
+            <div class="footer-left">
+                <img src="logoWhite.png" alt="logo">
+                <p>codemate mission is to help you find
+                    a roommate easily created by youcode
+                    students for youcode students.</p>
+            </div>
+            <div class="footer-right">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Contactus</a></li>
+                    <li><a href="#">Privacy</a></li>
+                    <li><a href="#">Term</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://code.iconify.design/2/2.1.0/iconify.min.js"></script>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <section class="left">
+        <img src="logoWhite.png" alt="logo">
+        <p>Le Lorem Ipsum est simplement du faux texte 
+            employé dans la composition et la mise en 
+            page avant impression. Le Lorem Ipsum est 
+            le faux texte standard de l'imprimerie depuis 
+            les années 1500, quand un imprimeur anonyme 
+            assembla ensemble des morceaux de texte pour 
+            réaliser un livre spécimen de polices de texte.</p>
+    </section>
+    <section class="right">
+        <img src="logo.png" alt="logo">
+        <h1>Login to your account</h1>
+    </section>
+</body>
+</html>
