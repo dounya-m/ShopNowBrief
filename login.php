5cbd8280d256296164c3ad01d0860033bf67a0c9
dounya-m <mellouki.dounya@gmail.com>
2022-01-22 01:16:55 +0100

    v1.01

--- login.php
+++ login.php
@@ -10,6 +10,7 @@
 </head>
 <body>
     <section class="left">
+        <div class="flou">
         <img src="logoWhite.png" alt="logo">
         <p>Le Lorem Ipsum est simplement du faux texte 
             employé dans la composition et la mise en 
@@ -18,10 +19,27 @@
             les années 1500, quand un imprimeur anonyme 
             assembla ensemble des morceaux de texte pour 
             réaliser un livre spécimen de polices de texte.</p>
+            </div>
     </section>
     <section class="right">
         <img src="logo.png" alt="logo">
+        <div class="title">
         <h1>Login to your account</h1>
+        <h3>See the lastest offers or create one</h3>
+        </div>
+        <div class="cases">
+        <div class="flex">
+        <label>User Name</label>
+        <input class="mail" type="email" id="mail" name="mail" placeholder="admin..">
+        </div>
+        <div class="flex">
+        <label>Password</label>
+        <input class="mail" type="password" id="password" name="password" placeholder="*****************">
+        <a href="#">Forgot your password?</a>
+        </div>
+        <div class="flex">
+			<button>Login</button>
+        </div>
     </section>
 </body>
 </html>
